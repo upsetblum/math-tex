e6b28692ba2968c2c3276d2b3836f975bb863963
Youssef <hammoudayoussef14@gmail.com>
2025-09-20 12:34:14 +0200

    Fix build errors: rename profile image and clean next.config.js

--- next.config.js
+++ next.config.js
@@ -10,9 +10,6 @@ const nextConfig = {
     unoptimized: true,
     remotePatterns: [],
   },
-  // Disable output file tracing which causes the build error
-  outputFileTracing: false,
-  swcMinify: true,
 }
 
 module.exports = nextConfig
